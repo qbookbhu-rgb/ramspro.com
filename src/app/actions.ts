f8cff849fe7a177f657b41ee67ddda9ad2868cb4
Anand kumar Mishra <qbookbhu@gmail.com>
2025-09-06 16:46:54 +0000

    Next

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -332,4 +332,3 @@ export async function updateOrderStatus(orderId: string, status: 'fulfilled' | '
         return { success: false, error: 'Could not update the order status. Please try again.' };
     }
 }
-    
