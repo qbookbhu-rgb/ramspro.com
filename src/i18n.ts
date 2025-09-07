3969745e6a5fafb764d1c83778099909c07637ad
Anand kumar Mishra <qbookbhu@gmail.com>
2025-09-07 04:44:46 +0000

    Okay thanks next

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,10 @@
-import {getRequestConfig} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {locales} from './lib/data';
 
+import {notFound} from 'next/navigation';
+import {getRequestConfig} from 'next-intl/server';
+ 
+// Can be imported from a shared config
+const locales = ['en', 'hi', 'bn', 'te'];
+ 
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
