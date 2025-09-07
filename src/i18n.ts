6e1edae320a429a751e0239ccf26d3226c6d5c21
Anand kumar Mishra <qbookbhu@gmail.com>
2025-09-07 04:15:24 +0000

    Okay thanks but please all fixed error with check all connectivity

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,14 @@
-
 import {getRequestConfig} from 'next-intl/server';
+import {locales} from './lib/data';
+import {notFound} from 'next/navigation';
  
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`./messages/${locale}.json`)).default
-}));
+export default getRequestConfig(async ({locale}) => {
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) {
+    notFound();
+  }
+ 
+  return {
+    messages: (await import(`../messages/${locale}.json`)).default
+  };
+});
