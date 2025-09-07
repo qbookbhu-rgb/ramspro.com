aa841737b60bf18dca96eeddf28a44a6abd20c64
Anand kumar Mishra <qbookbhu@gmail.com>
2025-09-07 05:19:24 +0000

    Error: Couldn't find next-intl config file. Please follow the instructio

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,5 @@
-
-import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
 import {locales} from './lib/data';
  
 export default getRequestConfig(async ({locale}) => {
