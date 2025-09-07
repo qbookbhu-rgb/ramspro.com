4e83c2d9ec0a353f5ab6fd18c318af84ae8f8a43
Anand kumar Mishra <qbookbhu@gmail.com>
2025-09-07 04:40:03 +0000

    Next

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,10 @@
 import {getRequestConfig} from 'next-intl/server';
-import {locales} from './lib/data';
 import {notFound} from 'next/navigation';
- 
+import {locales} from './lib/data';
+
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) {
-    notFound();
-  }
+  if (!locales.includes(locale as any)) notFound();
  
   return {
     messages: (await import(`../messages/${locale}.json`)).default
